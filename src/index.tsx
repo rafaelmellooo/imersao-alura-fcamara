efac900991ce792468db98dd207c8605ddc5c840
Rafael Mello <rafaelmello0715@outlook.com>
2021-10-11 03:44:52 -0300

    Adição das fontes Metropolis

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { App } from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-import "@fontsource/metropolis";
 import './styles/global.scss';
 
 ReactDOM.render(
