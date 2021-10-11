6630b16af77d0c5c082156514722d7f635ef4ae6
Rafael Mello <rafaelmello0715@outlook.com>
2021-10-11 03:45:28 -0300

    Pequenas modificações na Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,7 @@ import logo from '../assets/logo.png';
 
 import '../styles/header.scss';
 
-function Header()
-{
+function Header() {
     return (
         <header>
             <a href="/" target="_blank">
@@ -14,13 +13,13 @@ function Header()
 
             <nav>
                 <ul>
-                    <li><a href="/" target="_blank">quem somos</a></li>
+                    <li><a href="https://www.fcamara.com.br/quem-somos" target="_blank" rel="noreferrer">quem somos</a></li>
                     <li><a href="/" target="_blank">nossa atuação</a></li>
-                    <li><a href="/" target="_blank">cases</a></li>
-                    <li><a href="/" target="_blank">carreira</a></li>
-                    <li><a href="/" target="_blank">programa de formação</a></li>
-                    <li><a href="/" target="_blank">blog</a></li>
-                    <li><a href="/" target="_blank">contato</a></li>
+                    <li><a href="https://www.fcamara.com.br/casos-de-sucesso" target="_blank" rel="noreferrer">cases</a></li>
+                    <li><a href="https://jobs.kenoby.com/fcamara/" target="_blank" rel="noreferrer">carreira</a></li>
+                    <li><a href="https://www.fcamara.com.br/programa-formacao" target="_blank" rel="noreferrer">programa de formação</a></li>
+                    <li><a href="https://blog.fcamara.com.br/" target="_blank" rel="noreferrer">blog</a></li>
+                    <li><a href="https://www.fcamara.com.br/contato" target="_blank" rel="noreferrer">contato</a></li>
                 </ul>
             </nav>
 
